2157a805cc383e292e939776919bcc5e978bb0aa
Marcos Oliveira Sampaio <mosampaio@gmail.com>
2016-05-27 12:18:27 -0300

    Use twilio api to generates TwiML

--- app/Http/Controllers/Directory.php
+++ app/Http/Controllers/Directory.php
@@ -6,11 +6,15 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Services_Twilio_Twiml;
 
 class Directory extends Controller
 {
     public function search()
     {
-        return view('directory.notFound');
+        $message_response = new Services_Twilio_Twiml;
+        $message_response->message('We did not find the employee you\'re looking for');
+        return response($message_response, 200)->header('Content-Type', 'application/xml');
     }
+
 }
