497fa6be281ce349f27d864a8e774c4b1e5d27a8
Marcos Oliveira Sampaio <mosampaio@gmail.com>
2016-05-30 11:20:05 -0300

    Refactors controller methods

--- app/Http/Controllers/Directory.php
+++ app/Http/Controllers/Directory.php
@@ -16,31 +16,48 @@ class Directory extends Controller
         $name = $request->input('Body');
         $query = Employee::where('full_name', 'LIKE', '%' . $name . '%');
         $count = $query->count();
-        $twiml = new Services_Twilio_Twiml;
-        if ($count === 1) {
-            $employee = $query->first();
-            $twiml->message(collect([$employee->full_name, $employee->phone_number,
-                $employee->email])->implode("\n"));
-            return $this->_response($twiml);
+        if ($count == 1) {
+            return $this->singleResult($query);
         } elseif ($count > 1) {
-            $employees = $query->get();
-            $_SESSION["employees"] = $employees->map(function($employee, $key){
-                return $employee->email;
-            });
-            $employees_message = $employees->map(function($employee, $key) {
-                $option = $key+1;
-                return "$option for $employee->full_name";
-            });
-            $twiml->message(collect(['We found multiple people, reply with:',
-                    $employees_message,'Or start over'])->flatten()->implode("\n"));
-            return $this->_response($twiml);
+            return $this->multipleResults($query);
         } else {
-            $twiml->message('We did not find the employee you\'re looking for');
-            return $this->_response($twiml);
+            return $this->notFound();
         }
     }
 
-    private function _response($twiml)
+    private function singleResult($query)
+    {
+        $twiml = new Services_Twilio_Twiml;
+        $employee = $query->first();
+            $twiml->message(collect([$employee->full_name, $employee->phone_number,
+                $employee->email])->implode("\n"));
+        return $this->xmlResponse($twiml);
+    }
+
+    private function multipleResults($query)
+    {
+        $twiml = new Services_Twilio_Twiml;
+        $employees = $query->get();
+        $_SESSION["employees"] = $employees->map(function($employee, $key){
+            return $employee->email;
+        });
+        $employees_message = $employees->map(function($employee, $key) {
+            $option = $key+1;
+            return "$option for $employee->full_name";
+        });
+        $twiml->message(collect(['We found multiple people, reply with:',
+                $employees_message,'Or start over'])->flatten()->implode("\n"));
+        return $this->xmlResponse($twiml);
+    }
+
+    private function notFound()
+    {
+        $twiml = new Services_Twilio_Twiml;
+        $twiml->message('We did not find the employee you\'re looking for');
+        return $this->xmlResponse($twiml);
+    }
+
+    private function xmlResponse($twiml)
     {
         return response($twiml, 200)->header('Content-Type', 'application/xml');
     }
