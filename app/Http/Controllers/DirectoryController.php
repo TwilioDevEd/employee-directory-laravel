e724b48142df725aa7c5210813f85500560726ad
Mario Celi <mceli@stackbuilders.com>
2016-06-01 15:45:04 -0500

    Add phpcs check and correct style issues

--- app/Http/Controllers/DirectoryController.php
+++ app/Http/Controllers/DirectoryController.php
@@ -15,67 +15,83 @@ class DirectoryController extends Controller
     public function search(Request $request)
     {
         $body = $request->input('Body');
-        if ($this->isChoiceAnswer($body, $request)) {
-            return $this->selectedEmployee($body, $request);
+        if ($this->_isChoiceAnswer($body, $request)) {
+            return $this->_selectedEmployee($body, $request);
         }
 
         $query = Employee::where('full_name', 'LIKE', '%' . $body . '%');
         $count = $query->count();
         if ($count == 1) {
-            return $this->singleResult($query);
+            return $this->_singleResult($query);
         } elseif ($count > 1) {
-            return $this->multipleResults($query, $request);
+            return $this->_multipleResults($query, $request);
         } else {
-            return $this->notFound();
+            return $this->_notFound();
         }
     }
 
-    private function selectedEmployee($body, $request)
+    private function _selectedEmployee($body, $request)
     {
         $email = $request->session()->get('employees')->get($body - 1);
-        return $this->singleResult(Employee::where('email', $email));
+        return $this->_singleResult(Employee::where('email', $email));
     }
 
-    private function isChoiceAnswer($body, $request)
+    private function _isChoiceAnswer($body, $request)
     {
-        return is_numeric($body) 
-            && in_array(intval($body), 
-                range(1, $request->session()->get("employees")->count()));
+        return is_numeric($body)
+            && in_array(
+                intval($body),
+                range(1, $request->session()->get("employees")->count())
+            );
     }
 
-    private function singleResult($query)
+    private function _singleResult($query)
     {
         $twiml = new Services_Twilio_Twiml;
         $employee = $query->first();
-            $twiml->message(collect([$employee->full_name, $employee->phone_number,
-                $employee->email])->implode("\n"));
-        return $this->xmlResponse($twiml);
+            $twiml->message(
+                collect(
+                    [$employee->full_name, $employee->phone_number,
+                    $employee->email]
+                )->implode("\n")
+            );
+            return $this->_xmlResponse($twiml);
     }
 
-    private function multipleResults($query, $request)
+    private function _multipleResults($query, $request)
     {
         $twiml = new Services_Twilio_Twiml;
         $employees = $query->get();
-        $request->session()->put('employees', $employees->map(function($employee, $key){
-            return $employee->email;
-        }));
-        $employees_message = $employees->map(function($employee, $key) {
-            $option = $key + 1;
-            return "$option for $employee->full_name";
-        });
-        $twiml->message(collect(['We found multiple people, reply with:',
-                $employees_message, 'Or start over'])->flatten()->implode("\n"));
-        return $this->xmlResponse($twiml);
+        $request->session()->put(
+            'employees', $employees->map(
+                function ($employee, $key) {
+                    return $employee->email;
+                }
+            )
+        );
+        $employees_message = $employees->map(
+            function ($employee, $key) {
+                $option = $key + 1;
+                return "$option for $employee->full_name";
+            }
+        );
+        $twiml->message(
+            collect(
+                ['We found multiple people, reply with:',
+                $employees_message, 'Or start over']
+            )->flatten()->implode("\n")
+        );
+        return $this->_xmlResponse($twiml);
     }
 
-    private function notFound()
+    private function _notFound()
     {
         $twiml = new Services_Twilio_Twiml;
         $twiml->message('We did not find the employee you\'re looking for');
-        return $this->xmlResponse($twiml);
+        return $this->_xmlResponse($twiml);
     }
 
-    private function xmlResponse($twiml)
+    private function _xmlResponse($twiml)
     {
         return response($twiml, 200)->header('Content-Type', 'application/xml');
     }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,16 +13,17 @@
 
 use Illuminate\Http\Request;
 
-Route::group(['middleware' => ['web']], function () {
-    
-    Route::get('/', function () {
-        return 'Employee Directory Laravel';
-    });
-
-    Route::post(
-        '/directory/search/',
-        ['uses' => 'DirectoryController@search',
-        'as' => 'directory.search']
-    );
-
-});
+Route::group(
+    ['middleware' => ['web']], function () {
+        Route::get(
+            '/', function () {
+                return 'Employee Directory Laravel';
+            }
+        );
+        Route::post(
+            '/directory/search/',
+            ['uses' => 'DirectoryController@search',
+            'as' => 'directory.search']
+        );
+    }
+);
